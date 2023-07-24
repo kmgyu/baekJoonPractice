0d6cad1bf9617a795887b682280e11f474044857
kmgyu <lidersy961@gmail.com>
2023-07-24 16:53:41 +0900

    asdf

--- Geometry/가희는 그래플러야!!.py
+++ Geometry/가희는 그래플러야!!.py
@@ -2,3 +2,24 @@ n = int(input())
 nums = list(map(int, input().split()))
 k = int(input())
 
+f = []
+ans = 0
+
+if k - nums[0] == 0:
+    print("T")
+    exit()
+elif k - nums[0] < 0:
+    for i in range(1, n+1):
+        if k*i-nums[i-1] >= 0:
+            print("T")
+            exit()
+else:
+    for i in range(1, n+1):
+        if k*i-nums[i-1] <= 0:
+            print("T")
+            exit()
+print("F")
+
+
+# https://justicehui.github.io/sunrin-ps/2018/09/14/BOJ15916/
+# https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=chogahui05&logNo=221324025625
