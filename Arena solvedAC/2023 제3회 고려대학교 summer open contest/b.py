98c5a5aa5bef3cf5bd48e07f2f5651b4c5c70253
kmgyu <lidersy961@gmail.com>
2023-08-18 17:34:59 +0900

    aaaa

--- Arena solvedAC/2023 제3회 고려대학교 summer open contest/b.py
+++ Arena solvedAC/2023 제3회 고려대학교 summer open contest/b.py
@@ -3,16 +3,14 @@ n = int(input())
 if isqrt((n))**2 == n: print(-1)
 else:
     cnt = 0
-    acnt = 0
-    for i in range(1, isqrt(n)+1):
+    for i in range(1, (isqrt(n//2+1))+1):
         a = (n-i**2)
-        c = (n+i**2)
         if isqrt(a)**2 == a:
-            acnt+=1
-        if isqrt(c)**2 == c:
             cnt+=1
-    for i in range(isqrt(n)+1, n):
-        c = (n+i**2)
-        if isqrt(c)**2 == c:
-            cnt+=1
-    print(cnt+acnt//2)
+    for i in range(1, isqrt(n)+1):
+        if n % i == 0:
+            if (i + (n//i)) % 2 == 0:
+                if i == n//i: continue
+                cnt += 1
+    print(cnt)
+
