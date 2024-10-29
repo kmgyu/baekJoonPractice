ea9bdb82d9357703d29941f13d2d1087368302a5
kmgyu <lidersy961@gmail.com>
2024-10-29 10:34:00 +0900

    asdf

---
+++ 11835.py
@@ -0,0 +1,25 @@
+from sys import stdin
+input = stdin.readline
+N = int(input())
+A = list(map(int, input().rstrip().split())) + [0, 0]
+answer = 0
+for i in range(N):
+    if A[i+1] > A[i+2]:
+        cnt = min(A[i], A[i+1]-A[i+2])
+        answer += cnt*5
+        A[i] -= cnt
+        A[i+1] -= cnt
+
+        cnt = min(A[i], A[i+1], A[i+2])
+        answer += cnt*7
+        A[i] -= cnt
+        A[i+1] -= cnt
+        A[i+2] -= cnt
+    else:
+        cnt = min(A[i], A[i+1], A[i+2])
+        answer += cnt*7
+        A[i] -= cnt
+        A[i+1] -= cnt
+        A[i+2] -= cnt
+    answer += A[i]*3
+print(answer)
