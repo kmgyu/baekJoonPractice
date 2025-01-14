dbf2f1db1d99a67cdcedc2603807a16d007b75f6
kmgyu <lidersy961@gmail.com>
2025-01-14 14:50:04 +0900

    etc numbering

--- etc/printer queue.py
+++
@@ -1,6 +0,0 @@
-# t = int(input()) # test case
-# N, M = map(int, input().split()) # number of document, location
-# dc = list(map(int, input().split()))
-a = [1, 2, 3, 4, 5]
-
-# 완성못함
--- etc/바구니뒤집기.py
+++
@@ -1,6 +0,0 @@
-n, m = map(int, input().split())
-num = list(range(1, n+1))
-for i in range(m):
-    a, b = map(int,input().split())
-    num = num[:a-1] + list(reversed(num[a-1:b])) + num[b:]
-print(*num)
